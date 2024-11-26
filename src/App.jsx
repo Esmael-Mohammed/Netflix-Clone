bfc247ad0594dd7f08002f2ddcd06f5ad869e0c4
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2024-11-26 15:53:10 +0300

    deploynetlfy

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </>
   );
